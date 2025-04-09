7e1bb1c27d1aa123a5ccb7dd188e6ff547308aa3
Chinmoy94 <chinmoym66@gmail.com>
2025-04-09 21:58:20 +0530

    Routing component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-//import React, { useState } from 'react';
-//import Home from './pages/Home';
 import'./App.css';
-//import { CurrencyContext } from './context/CurrencyContext';
+
 import Routing from './Components/Routing/Routing';
 function App(){
-   // const [currency, setCurrency] = useState('usd');
-   return(
+     return(
     <>
-    {/* <CurrencyContext.Provider value={{currency, setCurrency}}> */}
-    <Routing />
-    {/* </CurrencyContext.Provider> */}
     
+    <Routing />
     </>
    );
 }
--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import CoinDetailsPage from "../../pages/CoinDetailsPage";
+import MainLayout from "../../pages/Layout";
 function Routing(){
    return(
        <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/details/:coinid" element={<CoinDetailsPage/>} />
+             <Route path="/" element={<MainLayout/>} >
+              <Route index element={<Home/>} />
+              <Route path="/details/:coinid" element={<CoinDetailsPage/>} /> 
+              </Route>
+              
+              </Routes>
         
-       </Routes>
+      
    );
 }
 export default Routing;
